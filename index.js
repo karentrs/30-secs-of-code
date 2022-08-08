05199446e7d2acb11c00a133f6911d2c64e8be95
karentrs <karen.trs25@gmail.com>
2022-08-07 23:53:46 -0500

    august 7th

---
+++ index.js
@@ -0,0 +1,7 @@
+//Comment
+//means you want to LOG 'Hello World' in the console. All statements end with a ';'.
+//This is my first JavaScript code.
+console.log('Hello World');
+//HTML includes the content while JavaScript implements how the content behaves.
+//we can either open command prompt, write cd space, then the file path, or you can open the terminal and follow
+//the same steps. 
