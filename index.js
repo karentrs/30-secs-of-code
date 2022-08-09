f36d9c54104472777274003aedcd426bc4d81b0a
karentrs <karen.trs25@gmail.com>
2022-08-08 23:57:35 -0500

    Update index.js
    August 8th

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 //Comment
 //means you want to LOG 'Hello World' in the console. All statements end with a ';'.
+
 //This is my first JavaScript code.
 console.log('Hello World');
+
 //HTML includes the content while JavaScript implements how the content behaves.
 //we can either open command prompt, write cd space, then the file path, or you can open the terminal and follow
 //the same steps. 
+
+//to declare a variable use 'let'/cannot be a reserved keyword/should be meaningful
+let name='Mosh';
+console.log(name);
+
+const interestRate = 0.3;
+// will get an error bc using const does not let you change the value of the variable
+interestRate = 1;
+console.log(interestRate);
+
+//primitives = string, numbers, booleans, undefined, and null
+let name = 'Mosh'; // String Literal
+let age = 30; //Number Literal
+let isApproved = true; //Boolean Literal
+let firstName = undefined; //value is undefined;
+let lastName = null; //use null when we want the value of the variable to be clear
+
+//javascript is a dynamic language. The value of a variable can change.
+
